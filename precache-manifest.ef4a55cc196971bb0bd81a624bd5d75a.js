61548bf7654bee54936658f0198a2c013d07942a
galit.sharon <galitsharon67@gmail.com>
2020-03-07 11:32:57 +0200

    Updates

---
+++ precache-manifest.ef4a55cc196971bb0bd81a624bd5d75a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce716490ad295bf70d1117eeaa0ee52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ca54d8797663d7c8e9a",
+    "url": "/static/css/main.bf34abb7.chunk.css"
+  },
+  {
+    "revision": "5dd2817142ca3db39634",
+    "url": "/static/js/2.df0769b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.df0769b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca54d8797663d7c8e9a",
+    "url": "/static/js/main.92a13d01.chunk.js"
+  },
+  {
+    "revision": "083a30154bb3d3135372",
+    "url": "/static/js/runtime-main.61042c8d.js"
+  },
+  {
+    "revision": "23759836fdfd617126176108a9949c23",
+    "url": "/static/media/multi.23759836.jpeg"
+  },
+  {
+    "revision": "a92cd407581d6dbfe07d781b0485e651",
+    "url": "/static/media/three.a92cd407.jpeg"
+  }
+]);
